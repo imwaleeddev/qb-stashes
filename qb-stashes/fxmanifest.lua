b43cd014e0c0dd4c7aeccfffc9a8a33214fc4b25
I'm Waleed <74907589+imwaleeddev@users.noreply.github.com>
2025-03-07 04:36:51 +0300

    Add files via upload

---
+++ qb-stashes/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+client_scripts { 'client/cl_*.lua' }
+shared_scripts { 'shared.lua' }
+server_scripts { 'server/sv_*.lua' }
+
+ui_page { 'html/index.html' }
+
+files { 'html/**/**' }
